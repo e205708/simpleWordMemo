18cf54fab913d6728992dcac89de629720da984b
Touyama Hiroto <e205708@ie.u-ryukyu.ac.jp>
2022-07-06 15:41:25 +0900

    Initial Commit

---
+++ speedmemoapp/speedmemoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  speedmemoappApp.swift
+//  speedmemoapp
+//
+//  Created by 當山寛人 on 2022/07/06.
+//
+
+import SwiftUI
+
+@main
+struct speedmemoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
